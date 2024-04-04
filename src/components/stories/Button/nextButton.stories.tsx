760046e4eb9ba39f4150559d28076257f0f6c33c
Kabieusz <123653582+KonradBlonka@users.noreply.github.com>
2024-04-04 19:56:41 +0200

    8: StoryBook adding more components

--- src/components/stories/Button/nextButton.stories.tsx
+++ src/components/stories/Button/nextButton.stories.tsx
@@ -15,8 +15,47 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+export const Link: Story = {
+  args: {
+    children: "Next",
+    variant: "link",
+    size: "lg",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    children: "Next",
+    variant: "outline",
+    size: "lg",
+  },
+};
+
 export const Default: Story = {
   args: {
     children: "Next",
+    variant: "default",
+    size: "lg",
+  },
+};
+export const Ghost: Story = {
+  args: {
+    children: "Next",
+    variant: "ghost",
+    size: "lg",
+  },
+};
+export const Destructive: Story = {
+  args: {
+    children: "Next",
+    variant: "destructive",
+    size: "lg",
+  },
+};
+export const Secondary: Story = {
+  args: {
+    children: "Next",
+    variant: "secondary",
+    size: "lg",
   },
 };
--- src/components/stories/Button/nextButton.tsx
+++ src/components/stories/Button/nextButton.tsx
@@ -1,16 +1,48 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { VariantProps, cva } from "class-variance-authority";
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 
-const buttonNextStyles = cva([
-  "text-white",
-  "w-full",
-  "rounded-md",
-  "disabled:cursor-not-allowed",
-]);
+const buttonNextStyles = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+          "text-blue-500 border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-white underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
 type ButtonProps = ComponentProps<"button"> &
   VariantProps<typeof buttonNextStyles>;
 
-export const nextButton = ({ ...props }: ButtonProps) => {
-  return <Button {...props} />;
-};
+export const nextButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant, size, className, ...props }, ref) => {
+    return (
+      <Button
+        ref={ref}
+        className={cn(buttonNextStyles({ variant, size, className }))}
+        {...props}
+      />
+    );
+  }
+);
